3ea168996e220fd2fd702912ffc3d50c3e813305
Hassan Amjad <hassanamjad9529@gmail.com>
2025-04-26 21:37:43 +0500

    hh

--- lib/main.dart
+++ lib/main.dart
@@ -1055,3 +1055,95 @@ class GradientBackground extends StatelessWidget {
 }
 }
 }
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: -280,
+          right: -200,
+          child: Container(
+            width: 400,
+            height: 400,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: RadialGradient(
+                center: Alignment.center,
+                radius: 2,
+                colors: [
+                  Color(0xff3F42FF),
+                  Colors.transparent,
+                ],
+              ),
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
+        ),
+        if (child != null) child!,
+      ],
+    );
+  }
+}
+}
+}
+}
+}
+}
+}
+}
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: -280,
+          right: -200,
+          child: Container(
+            width: 400,
+            height: 400,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: RadialGradient(
+                center: Alignment.center,
+                radius: 2,
+                colors: [
+                  Color(0xff3F42FF),
+                  Colors.transparent,
+                ],
+              ),
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
+        ),
+        if (child != null) child!,
+      ],
+    );
+  }
+}
+}
+}
+}
+}
+}
+}
+}
