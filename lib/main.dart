2e20b114839c4876e0279b97e23c463664a8d826
Hassan Amjad <hassanamjad9529@gmail.com>
2025-05-17 18:02:31 +0500

    hh

--- lib/main.dart
+++ lib/main.dart
@@ -943,8 +943,6 @@ class GradientBackground extends StatelessWidget {
                 radius: 3.5,
                 radius: 3.5,
                 radius: 3.5,
-                radius: 3.5,
-                radius: 3.5,
                 colors: [
                   Color(0xff3F42FF),
                   Colors.transparent,
