7e6a958be30aefef37336f46e439c450f264ace7
Hassan Amjad <hassanamjad9529@gmail.com>
2025-06-02 14:30:35 +0500

    hh

--- lib/main.dart
+++ lib/main.dart
@@ -19,27 +19,6 @@ class MyApp extends StatelessWidget {
 //         // This is the theme of your application.
 //         //
 //         // TRY THIS: Try running your application with "flutter run". You'll see
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-//         // This is the theme of your application.
-//         //
-//         // TRY THIS: Try running your application with "flutter run". You'll see
 //         // the application has a purple toolbar. Then, without quitting the app,
 //         // try changing the seedColor in the colorScheme below to Colors.green
 //         // and then invoke "hot reload" (save your changes or press the "hot
