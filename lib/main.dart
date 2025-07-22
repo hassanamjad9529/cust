211ee5bc01023cff7669c3fa968e6dcc0b91cdad
Hassan Amjad <hassanamjad9529@gmail.com>
2025-07-22 22:48:44 +0500

    hH

--- lib/main.dart
+++ lib/main.dart
@@ -3117,8 +3117,6 @@ class GradientBackground extends StatelessWidget {
           decoration: const BoxDecoration(
           decoration: const BoxDecoration(
           decoration: const BoxDecoration(
-          decoration: const BoxDecoration(
-          decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
