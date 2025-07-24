0bb38c5f46a3b8efdd9652705797745ac7065586
Hassan Amjad <hassanamjad9529@gmail.com>
2025-07-24 10:17:06 +0500

    H

--- lib/main.dart
+++ lib/main.dart
@@ -3187,32 +3187,6 @@ class GradientBackground extends StatelessWidget {
             ),
           ),
         ),
-        Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFF12064F),
-                Color(0xFF12064F),
-              ],
-              stops: [0.0, 1.0],
-            ),
-          ),
-        ),
-        Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFF12064F),
-                Color(0xFF12064F),
-              ],
-              stops: [0.0, 1.0],
-            ),
-          ),
-        ),
         Positioned(
           top: -250,
           left: -180,
