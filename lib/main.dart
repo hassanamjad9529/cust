aab548765298b8e9ff8d504caa032c46ff68436f
Hassan Amjad <hassanamjad9529@gmail.com>
2025-10-09 19:46:23 +0500

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -224,14 +224,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
 //         // This is the theme of your application.
 //         //
 //         // TRY THIS: Try running your application with "flutter run". You'll see
