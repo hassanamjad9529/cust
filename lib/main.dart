56f6ef0d5a5602f5a3f145b734557b2a00d53bed
Hassan Amjad <hassanamjad9529@gmail.com>
2026-01-01 22:45:24 +0500

    Gg

--- lib/main.dart
+++ lib/main.dart
@@ -1131,36 +1131,6 @@ class _SplashViewState extends State<SplashView>
     );
       ),
     );
-    );
-      ),
-    );
-
-    // Text reveal animation (faster)
-      ),
-    );
-      ),
-    );
-
-    // Text reveal animation (faster)
-      ),
-    );
-      ),
-    );
-    );
-      ),
-    );
-
-    // Text reveal animation (faster)
-      ),
-    );
-      ),
-    );
-
-    // Text reveal animation (faster)
-      ),
-    );
-      ),
-    );
 
     // Text reveal animation (faster)
       ),
